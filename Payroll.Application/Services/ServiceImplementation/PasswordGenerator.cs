fcafbef67eb4afc4990b8b83c55401cac2477071
Munali1 <munali.shakya@gmail.com>
2025-04-10 13:49:51 +0545

    Password Generation According to Identity

--- Payroll.Application/Services/ServiceImplementation/PasswordGenerator.cs
+++ Payroll.Application/Services/ServiceImplementation/PasswordGenerator.cs
@@ -11,10 +11,31 @@ namespace Payroll.Application.Services.ServiceImplementation
     {
         public string GeneratePassword(int length = 10)
         {
-            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$!";
+            if (length < 6) throw new ArgumentException("Password length must be at least 6.");
+
+            const string upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            const string lower = "abcdefghijklmnopqrstuvwxyz";
+            const string digits = "0123456789";
+            const string special = "@#$!";
+            const string all = upper + lower + digits + special;
+
             var random = new Random();
-            return new string(Enumerable.Repeat(chars, length)
-                .Select(s => s[random.Next(s.Length)]).ToArray());
+            var password = new StringBuilder();
+
+            // Ensure at least one character from each required category
+            password.Append(upper[random.Next(upper.Length)]);
+            password.Append(lower[random.Next(lower.Length)]);
+            password.Append(digits[random.Next(digits.Length)]);
+            password.Append(special[random.Next(special.Length)]);
+
+            // Fill the rest with random chars
+            for (int i = 4; i < length; i++)
+            {
+                password.Append(all[random.Next(all.Length)]);
+            }
+
+            return new string(password.ToString().OrderBy(x => random.Next()).ToArray());
         }
+
     }
 }
